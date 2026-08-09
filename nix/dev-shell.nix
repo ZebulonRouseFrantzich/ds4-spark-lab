462a71f3b3bcd82bab14711f26db8c4e3f0d8deb
Zebulon Rouse-Frantzich <zebulonfrantzich@gmail.com>
2026-08-08 19:06:00 -0500

    fix(tooling): isolate Git SSH config in dev shell

--- nix/dev-shell.nix
+++ nix/dev-shell.nix
@@ -1,7 +1,17 @@
 { pkgs }:
+let
+  gitSsh = pkgs.writeShellScript "ds4-git-ssh" ''
+    if [[ -r "''${HOME}/.ssh/config" ]]; then
+      exec ${pkgs.openssh}/bin/ssh -F "''${HOME}/.ssh/config" "$@"
+    fi
+
+    exec ${pkgs.openssh}/bin/ssh -F /dev/null "$@"
+  '';
+in
 pkgs.mkShellNoCC {
   packages = import ./tooling.nix { inherit pkgs; };
 
+  GIT_SSH = "${gitSsh}";
   UV_PYTHON_DOWNLOADS = "never";
   UV_NO_MANAGED_PYTHON = "1";
   UV_PYTHON = "${pkgs.python3}/bin/python";
